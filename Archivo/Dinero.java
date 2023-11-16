f243071574d1ece3bc62ca6db70dc40104e9fa1b
Contreras71399 <145361635+Contreras71399@users.noreply.github.com>
2023-11-16 00:13:35 -0600

    Create Dinero.java

---
+++ Archivo/Dinero.java
@@ -0,0 +1,36 @@
+package Cajero;
+
+import java.io.Serializable;
+
+public class Billete implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int denominacion;
+    private int cantidad;
+
+    public Billete(int denominacion, int cantidad) {
+        this.denominacion = denominacion;
+        this.cantidad = cantidad;
+    }
+
+    public int getDenominacion() {
+        return denominacion;
+    }
+
+    public void setDenominacion(int denominacion) {
+        this.denominacion = denominacion;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "$" + denominacion + " - Cantidad: " + cantidad;
+    }
+}
