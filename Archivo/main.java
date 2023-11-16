4f3122c4c1fa8b6cd3fe621bdf34271db54b770f
Contreras71399 <145361635+Contreras71399@users.noreply.github.com>
2023-11-16 00:17:23 -0600

    Create main.java

---
+++ Archivo/main.java
@@ -0,0 +1,29 @@
+package Cajero;
+import java.util.Scanner;
+
+public class CajeroAutomaticoMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CajeroAutomatico cajero = new CajeroAutomatico();
+
+        int opcion;
+        do {
+            System.out.println("\nMenú Principal:");
+            System.out.println("1. Iniciar sesion");
+            System.out.println("2. Salir");
+            System.out.print("Ingrese la opción: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    cajero.Iniciar();
+                    break;
+                case 2:
+                    System.out.println("Sesión finalizada");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente nuevamente.");
+            }
+        } while (opcion != 2);
+    }
+}
